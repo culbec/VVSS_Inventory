0127131b5ff06b95ef05fd437e20b4d4c37c615f
culbec <profir.alexx@gmail.com>
2025-05-07 15:02:11 +0300

    removed opens of not existing modules in test.

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -4,12 +4,6 @@ module inventoryTest {
     requires org.junit.jupiter.api;
     requires org.mockito;
 
-    opens inventory to org.junit.platform.commons;
-    opens inventory.model to org.junit.platform.commons;
-    opens inventory.repository to org.junit.platform.commons;
-    opens inventory.validator to org.junit.platform.commons;
-    opens inventory.service to org.junit.platform.commons;
-
     opens part.test to org.junit.platform.commons;
     opens product.test to org.junit.platform.commons;
     opens inventory.test to org.junit.platform.commons;
